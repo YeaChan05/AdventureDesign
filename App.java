c40512ba0196be8436c11cd1c6eb4a208c6fd6c4
YeaChan05 <qkenrdl05@naver.com>
2021-11-28 01:17:33 +0900

    Add fxml files - GameFrame, IntroFrame

--- App.java
+++ App.java
@@ -1,7 +1,8 @@
-/*
+
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -9,6 +10,7 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -21,118 +23,8 @@ import javafx.scene.control.*;
 import javafx.scene.text.*;
 
 public class App extends Application implements EventHandler<ActionEvent> {
-    static Stage window;
-    static Scene scene1;
-    static Scene scene2;
-
-    @Override
-    public void init() throws Exception {
-        // TODO Auto-generated method stub
-        super.init();
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-       //Parent root =FXMLLoader.load(getClass().getResource("MainFrame.fxml"));
-       window = stage;
-       window.setWidth(1500);
-       window.setHeight(800);
-       window.setX(200);
-       window.setY(50);
-      
-       Label label1 = new Label("게임 시작화면");
-       //Label label2 = new Label("this is a second scene!");
-
-       Button button_login = new Button("Login");
-       button_login.setOnAction(e->Login());
-
-       VBox layout_gamestart = new VBox(100);
-       layout_gamestart.getChildren().addAll(label1,button_login);
-       scene1 = new Scene(layout_gamestart);
-       layout_gamestart.setAlignment(Pos.CENTER);
-
-    //    Button button2 = new Button("Fuck out to the scene1");
-    //    button2.setOnAction(e->display("응애","this is a test"));
-
-    //    StackPane layout2 = new StackPane();
-    //    layout2.getChildren().addAll(label2,button2);
-    //    scene2 = new Scene(layout2);
-    //    layout2.setAlignment(Pos.CENTER);
-
-       window.setScene(scene1);
-       stage.setTitle("공대생 키우기");
-       stage.show();
-    }
-
-    @Override
-    public void stop() throws Exception {
-        // TODO Auto-generated method stub
-        super.stop();
-    }
     
-    public static void display(String title, String message){
-        window=new Stage();
 
-        window.initModality(Modality.APPLICATION_MODAL);
-        window.setTitle(title);
-        window.initStyle(StageStyle.TRANSPARENT);
-        
-
-        Label label=new Label();
-        label.setText(message);
-        Button closeButton =new Button("Close the window");
-        closeButton.setOnAction(e->window.close());
-        
-        StackPane layout = new StackPane();
-        layout.getChildren().add(closeButton);
-        layout.setAlignment(Pos.CENTER);
-
-        Scene scene = new Scene(layout,1500,1000);
-        window.setScene(scene);
-        window.showAndWait();
-    }
-
-    public static void Login(){
-        window = new Stage();
-        GridPane grid = new GridPane();
-        Scene scene = new Scene(grid, 300, 275);
-
-        Text scenetitle = new Text("Welcome"); 
-        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        grid.add(scenetitle, 0, 0, 2, 1);
-
-        Label userName = new Label("User Name:"); 
-        grid.add(userName, 0, 1);
-
-        TextField userTextField = new TextField(); 
-        grid.add(userTextField, 1, 1);
-        String id_text=userTextField.getText();
-
-        Label pw = new Label("Password:"); 
-        grid.add(pw, 0,2);
-
-        PasswordField pwBox = new PasswordField(); 
-        grid.add(pwBox, 1, 2);
-        String pw_text=userTextField.getText();
-
-        Button loginAccount = new Button("Login");
-        grid.add(loginAccount,1,3);
-        loginAccount.setOnAction(e->checkAccount(id_text,pw_text));
-
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
-
-        window.setScene(scene);
-        window.show();
-
-    }
-
-    public static void checkAccount(String id, String pw){
-
-
-    }
 
     @Override
     public void handle(ActionEvent arg0) {
@@ -143,5 +35,10 @@ public class App extends Application implements EventHandler<ActionEvent> {
     public static void Launch() {
         launch();
     }
+
+    @Override
+    public void start(Stage primeStage) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
 }
-*/
