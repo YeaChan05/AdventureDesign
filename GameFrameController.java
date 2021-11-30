28cb676a6f30f71fb7cf1f5cad296accb197b0d4
kiminchan <kiminchan12341@gmail.com>
2021-11-30 22:15:13 +0900

    add Game Frame fxml, GameFrameController, add resouces png

---
+++ GameFrameController.java
@@ -0,0 +1,109 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+public class GameFrameController {
+	
+	    @FXML
+	    private ResourceBundle resources;
+
+	    @FXML
+	    private URL location;
+
+	    @FXML
+	    private Label activenessLabel;
+
+	    @FXML
+	    private ProgressBar activenessProgressBar;
+
+	    @FXML
+	    private TextField activenessTextField;
+
+	    @FXML
+	    private Button downtownEventButton;
+
+	    @FXML
+	    private Label healthLabel;
+
+	    @FXML
+	    private ProgressBar healthProgressBar;
+
+	    @FXML
+	    private TextField healthTextField;
+
+	    @FXML
+	    private Label intelligenceLabel;
+
+	    @FXML
+	    private ProgressBar intelligenceProgressBar;
+
+	    @FXML
+	    private TextField intelligenceTextField;
+
+	    @FXML
+	    private Label interestLabel;
+
+	    @FXML
+	    private ProgressBar interestProgressBar;
+
+	    @FXML
+	    private TextField interestTextField;
+
+	    @FXML
+	    private Label moneyLabel;
+
+	    @FXML
+	    private TextField moneyTextField;
+
+	    @FXML
+	    private ImageView optionImageView;
+
+	    @FXML
+	    private Label semesterLabel;
+
+	    @FXML
+	    private TextField semesterTextField;
+
+	    @FXML
+	    private Button universityEventButton;
+
+	    @FXML
+	    private ImageView universityImageView;
+
+	    @FXML
+	    private ImageView userImageView;
+
+	    @FXML
+	    void initialize() {
+	        assert activenessLabel != null : "fx:id=\"activenessLabel\" was not injected: check your FXML file 'Untitled'.";
+	        assert activenessProgressBar != null : "fx:id=\"activenessProgressBar\" was not injected: check your FXML file 'Untitled'.";
+	        assert activenessTextField != null : "fx:id=\"activenessTextField\" was not injected: check your FXML file 'Untitled'.";
+	        assert downtownEventButton != null : "fx:id=\"downtownEventButton\" was not injected: check your FXML file 'Untitled'.";
+	        assert healthLabel != null : "fx:id=\"healthLabel\" was not injected: check your FXML file 'Untitled'.";
+	        assert healthProgressBar != null : "fx:id=\"healthProgressBar\" was not injected: check your FXML file 'Untitled'.";
+	        assert healthTextField != null : "fx:id=\"healthTextField\" was not injected: check your FXML file 'Untitled'.";
+	        assert intelligenceLabel != null : "fx:id=\"intelligenceLabel\" was not injected: check your FXML file 'Untitled'.";
+	        assert intelligenceProgressBar != null : "fx:id=\"intelligenceProgressBar\" was not injected: check your FXML file 'Untitled'.";
+	        assert intelligenceTextField != null : "fx:id=\"intelligenceTextField\" was not injected: check your FXML file 'Untitled'.";
+	        assert interestLabel != null : "fx:id=\"interestLabel\" was not injected: check your FXML file 'Untitled'.";
+	        assert interestProgressBar != null : "fx:id=\"interestProgressBar\" was not injected: check your FXML file 'Untitled'.";
+	        assert interestTextField != null : "fx:id=\"interestTextField\" was not injected: check your FXML file 'Untitled'.";
+	        assert moneyLabel != null : "fx:id=\"moneyLabel\" was not injected: check your FXML file 'Untitled'.";
+	        assert moneyTextField != null : "fx:id=\"moneyTextField\" was not injected: check your FXML file 'Untitled'.";
+	        assert optionImageView != null : "fx:id=\"optionImageView\" was not injected: check your FXML file 'Untitled'.";
+	        assert semesterLabel != null : "fx:id=\"semesterLabel\" was not injected: check your FXML file 'Untitled'.";
+	        assert semesterTextField != null : "fx:id=\"semesterTextField\" was not injected: check your FXML file 'Untitled'.";
+	        assert universityEventButton != null : "fx:id=\"universityEventButton\" was not injected: check your FXML file 'Untitled'.";
+	        assert universityImageView != null : "fx:id=\"universityImageView\" was not injected: check your FXML file 'Untitled'.";
+	        assert userImageView != null : "fx:id=\"userImageView\" was not injected: check your FXML file 'Untitled'.";
+
+	    }
+
+}
+
+
