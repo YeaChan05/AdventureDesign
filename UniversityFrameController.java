b03d2fe756b5f092795c6db767bea50b36762792
YeaChan05 <qkenrdl05@naver.com>
2021-12-05 17:45:05 +0900

    toggleButton 설정 안됨

--- UniversityFrameController.java
+++ UniversityFrameController.java
@@ -3,11 +3,12 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 public class UniversityFrameController {
-
+    private GameFrameController gameFrameController=new GameFrameController();
     @FXML
     private ResourceBundle resources;
 
@@ -18,40 +19,37 @@ public class UniversityFrameController {
     private AnchorPane anchorpane;
 
     @FXML
-    private Button clubactivitiesButton;
+    private ToggleButton clubactivitiesButton;
 
     @FXML
-    private Button culturelecture1Button;
+    private ToggleButton culturelecture1Button;
 
     @FXML
-    private Button culturelecture2Button;
+    private ToggleButton culturelecture2Button;
 
     @FXML
-    private Button hiddeneventButton;
+    private ToggleButton hiddeneventButton;
 
     @FXML
-    private Label lecutreLabel;
+    private ToggleButton majorlecture1Button;
 
     @FXML
-    private Button majorlecture1Button;
+    private ToggleButton majorlecture2Button;
 
     @FXML
-    private Button majorlecture2Button;
+    private ToggleButton majorlecture3Button;
 
     @FXML
-    private Button majorlecture3Button;
+    private ToggleButton majorlecture4Button;
 
     @FXML
-    private Button majorlecture4Button;
-
-    @FXML
-    private Button specialeventButton;
+    private ToggleButton specialeventButton;
 
     @FXML
     private Button studyButton;
 
     @FXML
-    private Label universityLabel;
+    private Text universityText;
 
     @FXML
     void OnclubactivitiesButton(ActionEvent event) {
@@ -59,13 +57,8 @@ public class UniversityFrameController {
     }
 
     @FXML
-    void Onculturelecture1Button(ActionEvent event) {
-
-    }
-
-    @FXML
-    void Onculturelecture2Button(ActionEvent event) {
-
+    void OnculturelectureButton(ActionEvent event) {
+        
     }
 
     @FXML
@@ -74,25 +67,11 @@ public class UniversityFrameController {
     }
 
     @FXML
-    void Onmajorlecture1Button(ActionEvent event) {
-
+    void OnmajorlectureButton(ActionEvent event) {
+        gameFrameController.adjHealth(1);
+        System.out.println(gameFrameController.getCon().getCharacter().getHealth());
     }
-
-    @FXML
-    void Onmajorlecture2Button(ActionEvent event) {
-
-    }
-
-    @FXML
-    void Onmajorlecture3Button(ActionEvent event) {
-
-    }
-
-    @FXML
-    void Onmajorlecture4Button(ActionEvent event) {
-
-    }
-
+    
     @FXML
     void OnspecialeventButton(ActionEvent event) {
 
@@ -110,15 +89,20 @@ public class UniversityFrameController {
         assert culturelecture1Button != null : "fx:id=\"culturelecture1Button\" was not injected: check your FXML file 'UniversityFrame.fxml'.";
         assert culturelecture2Button != null : "fx:id=\"culturelecture2Button\" was not injected: check your FXML file 'UniversityFrame.fxml'.";
         assert hiddeneventButton != null : "fx:id=\"hiddeneventButton\" was not injected: check your FXML file 'UniversityFrame.fxml'.";
-        assert lecutreLabel != null : "fx:id=\"lecutreLabel\" was not injected: check your FXML file 'UniversityFrame.fxml'.";
         assert majorlecture1Button != null : "fx:id=\"majorlecture1Button\" was not injected: check your FXML file 'UniversityFrame.fxml'.";
         assert majorlecture2Button != null : "fx:id=\"majorlecture2Button\" was not injected: check your FXML file 'UniversityFrame.fxml'.";
         assert majorlecture3Button != null : "fx:id=\"majorlecture3Button\" was not injected: check your FXML file 'UniversityFrame.fxml'.";
         assert majorlecture4Button != null : "fx:id=\"majorlecture4Button\" was not injected: check your FXML file 'UniversityFrame.fxml'.";
         assert specialeventButton != null : "fx:id=\"specialeventButton\" was not injected: check your FXML file 'UniversityFrame.fxml'.";
         assert studyButton != null : "fx:id=\"studyButton\" was not injected: check your FXML file 'UniversityFrame.fxml'.";
-        assert universityLabel != null : "fx:id=\"universityLabel\" was not injected: check your FXML file 'UniversityFrame.fxml'.";
+        assert universityText != null : "fx:id=\"universityText\" was not injected: check your FXML file 'UniversityFrame.fxml'.";
 
     }
 
+    public void setController(GameFrameController gameFrameController){
+        this.gameFrameController = gameFrameController;
+    }
+    public GameFrameController getController(){
+        return this.gameFrameController;
+    }
 }
