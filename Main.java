1e7cb654d8b9b86a4f2c1a2c8b21588ad8e7a20b
YeaChan05 <qkenrdl05@naver.com>
2021-12-12 03:10:31 +0900

    final

--- Main.java
+++ Main.java
@@ -1,13 +1,5 @@
 import java.util.Scanner;
 public class Main{
-	
-	static Scanner sin = new Scanner(System.in);
-    public static String getStrInput(String msg) {
-        Scanner in = new Scanner(System.in);
-		System.out.println(msg);
-		return in.nextLine();
-	}
-    
     public static void main(String[] args) throws Exception{
         IntroFrameController.Launch();
     }
